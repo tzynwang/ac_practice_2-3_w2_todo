6ee992885a62b674672f636002a8fd2610a3dd54
tzyn (mac) <tzyn.wang@gmail.com>
2021-05-09 21:06:51 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -10,6 +10,26 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const todoSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  }
+})
+module.exports = mongoose.model('Todo', todoSchema)
+
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error')
+})
+db.once('open', () => {
+  console.log('mongodb connected')
+})
+
 app.get('/', (req, res) => {
   res.render('index')
 })
