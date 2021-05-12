65ae508cd59960aac4ffb1da1e8b1df4d7855306
tzyn (mac) <tzyn.wang@gmail.com>
2021-05-12 19:43:08 +0800

    feat: todo status checker

--- models/todo.js
+++ models/todo.js
@@ -4,6 +4,10 @@ const todoSchema = new Schema({
   name: {
     type: String,
     required: true
+  },
+  isDone: {
+    type: Boolean,
+    default: false
   }
 })
 module.exports = mongoose.model('Todo', todoSchema)
